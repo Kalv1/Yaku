0814a9d79ca39e6dd22c315e1390fef974949dca
Kalv1 <calvin.lambert@protonmail.com>
2024-03-05 21:44:15 +0100

    modify explanation card

--- components/card/explanation.vue
+++ components/card/explanation.vue
@@ -1,3 +1,9 @@
+<script setup>
+const props = defineProps({
+  title: String,
+});
+</script>
+
 <template>
   <div
     class="w-full h-full bg-black shadow-sm rounded overflow-hidden relative text-white"
@@ -10,8 +16,8 @@
       class="h-full w-full p-2.5 z-10 relative flex flex-col justify-between"
     >
       <div class="flex items-center gap-3">
-        <h1 class="font-boska">Calendrier</h1>
-        <div class="w-full h-[1px] bg-white" />
+        <h1 class="font-boska">{{ title }}</h1>
+        <div v-if="title" class="w-full h-[1px] bg-white" />
       </div>
       <p class="text-left text-[0.6rem] text-left">
         <slot />
--- pages/calendar.vue
+++ pages/calendar.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="w-full h-full grid grid-cols-6 grid-rows-5 gap-3">
-    <card-explanation class="col-span-3 row-span-1">
+    <card-explanation class="col-span-3 row-span-1" title="Calendrier">
       Profite de la flexibilité du télétravail à l'agence : <br />
       Réserve dès maintenant tes créneaux pour travailler où tu veux, quand tu
       veux !
